e79f7f8faccf5d7ed2d373d4d75ed2252221e222
dchao <dchao@caltech.edu>
2016-07-28 13:49:22 -0700

    Removed (commented out) mmiss_prime2 sideband.

--- reserve_sideband/create_sideband_views.sql
+++ reserve_sideband/create_sideband_views.sql
@@ -6,9 +6,10 @@ CREATE MATERIALIZED VIEW sideband_data AS
 SELECT 
   eid, 
   eextra_sideband,
-  mmiss2prime_sideband,
+--  mmiss2prime_sideband,
   sighp3_sideband,
-  (eextra_sideband=1 OR mmiss2prime_sideband=1 OR sighp3_sideband=1)::integer AS sideband
+--  (eextra_sideband=1 OR mmiss2prime_sideband=1 OR sighp3_sideband=1)::integer AS sideband
+  (eextra_sideband=1 OR sighp3_sideband=1)::integer AS sideband
 FROM reserved_data;
 
 CREATE INDEX ON sideband_data (eid);
@@ -26,14 +27,15 @@ CREATE MATERIALIZED VIEW sideband_generic AS
 SELECT 
   eid, 
   S.eextra_sideband AS eextra_sideband,
-  S.mmiss2prime_sideband AS mmiss2prime_sideband,
+--  S.mmiss2prime_sideband AS mmiss2prime_sideband,
   S.sighp3_sideband AS sighp3_sideband,
-  (eextra_sideband=1 OR mmiss2prime_sideband=1 OR sighp3_sideband=1)::integer AS sideband
+--  (eextra_sideband=1 OR mmiss2prime_sideband=1 OR sighp3_sideband=1)::integer AS sideband
+  (eextra_sideband=1 OR sighp3_sideband=1)::integer AS sideband
 FROM 
   (SELECT 
      eid,
      (eextra >= 3)::integer AS eextra_sideband,
-     (mmiss2prime <= -15)::integer AS mmiss2prime_sideband,
+--     (mmiss2prime <= -15)::integer AS mmiss2prime_sideband,
      (sig_hp3 >= 2.3)::integer AS sighp3_sideband
    FROM candidate_optimized_events_generic) AS S;
 
